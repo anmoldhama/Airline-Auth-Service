a89c4dc18a318c17cf25becb33317cea033cd45e
anmol_dhama <anmol.dhama@rapidshyp.com>
2025-01-28 15:18:18 +0530

    FEAT: first commitinitial setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {PORT} = require('./config/serverConfig');
+const app = express();
+
+function prepareAndStartServer(){
+   app.listen(PORT,()=>{
+      console.log('Server started at port: ',PORT);
+   })
+}
+
+prepareAndStartServer();
